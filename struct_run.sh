852dc44da8ca953b9d8b2fb2f4f4024807ea382b
singerinsky@gmail.com <root@ecs-dda5.novalocal>
2019-09-04 15:49:04 +0800

    add new file

--- struct_run.sh
+++ struct_run.sh
@@ -1,4 +1,5 @@
 rm p.zip
 rm out -rf
 zip -r p.zip spark-kafka.py
-spark-submit --driver-class-path kafka-clients-2.3.0.jar --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.3 --master local[1] --py-files p.zip spark-kafka.py 
+#spark-submit --driver-class-path kafka-clients-2.3.0.jar --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.3 --master local[1] --py-files p.zip spark-kafka.py 
+/opt/software/spark-2.4.4-bin-hadoop2.7/bin/spark-submit --driver-class-path kafka-clients-2.3.0.jar --jars mysql-connector-java-5.1.47.jar --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.4 --master local[1] --py-files p.zip spark-kafka.py 
